268c6a1c5cf24e5b465aa69df2c1fcec4bb19861
nilammone <nilammone@ldblao.la>
2021-03-03 10:58:38 +0700

    bk20210303

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\RedirectResponse;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
 });
+
+Auth::routes();
+// Auth::routes(['register' => false]); -- hidden register page
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
